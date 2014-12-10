00d2014e88f7f6ca891d3a80ad0128ebfbdded0a
Lenneke Jong <Lenneke.Jong@synchrotron.org.au>
2014-12-10 14:53:43 +1100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='sxri_pipeline',
+    version='0.1',
+    author = "Lenneke Jong",
+    author_email='pdviper@synchrotron.org.au',
+    description = "sxri_pipeline",
+    packages=find_packages(),
+    license='LICENSE',
+    install_requires=[
+	    "numpy",
+        "h5py",
+        "pyNADIA"
+    ],
+)
